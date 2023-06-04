35978a2e8c44dda438aac388c86dab8b6a1f498f
ChooseImage <chooseimage0125@gmail.com>
2023-06-04 18:30:06 -0400

    implemented datable

--- scripts/jsonToTable.js
+++ scripts/jsonToTable.js
@@ -1,7 +1,7 @@
 //const {storeData} = require('../assets/store-location.json')
 let storeData;
 
-fetch('../../assets/store-location.json')
+fetch('../../assets/storeLocation-updated.json')
   .then(response => response.json())
   .then((data) => {
                      storeData = data;
@@ -10,7 +10,6 @@ fetch('../../assets/store-location.json')
   .catch(error => console.log(error));
 
 function convert() {
- 
  // Get the container element where the table will be inserted
  let container = document.getElementById("Locations_wrapper");
  // Create the table element
@@ -18,10 +17,11 @@ function convert() {
  table.setAttribute("id", "Locations");
  table.setAttribute("class", "Locations dataTable");
  table.setAttribute("style", "width:100%");
- table.setAttribute("aria-describedby", "Locations_info");
+ table.classList.add('stripe');
+ table.classList.add('row-border');
+ //table.setAttribute("aria-describedby", "Locations_info");
  // Get the keys (column names) of the first object in the JSON data
  let cols = Object.keys(storeData[0]);
-
  // Create the header element
  let thead = document.createElement("thead");
  //table.appendChild(thead)
@@ -32,15 +32,15 @@ function convert() {
  cols.forEach((item) => {
     let th = document.createElement("th");
     th.setAttribute("tabindex", "0");
-    th.setAttribute("class", "sorting sorting_asc");
-    th.setAttribute("aria-controls", "Locations");
+    //th.setAttribute("class", "sorting sorting_asc"); //Sorting Params
+    //th.setAttribute("aria-controls", "Locations");
     th.setAttribute("rowspan", "1");
 
     th.setAttribute("colspan", "1");
-    th.setAttribute("aria-sort", "ascending");
+    //th.setAttribute("aria-sort", "ascending");
     th.setAttribute("rowspan", "1");
     th.setAttribute("style", "width:72.3125px")
-    th.setAttribute("aria-label", `${item}: activate to sort column descending`)
+    //th.setAttribute("aria-label", `${item}: activate to sort column descending`)
     th.innerText = item; // Set the column name as the text of the header cell
     tr.appendChild(th); // Append the header cell to the header row
  });
@@ -66,6 +66,36 @@ function convert() {
  });
  container.appendChild(table) // Append the table to the container element
 }
+
+// Search
+/*
+https://datatables.net/reference/option/language
+*/
+$(document).ready(function () {
+    $('#Locations').DataTable({
+      "lengthChange": true,
+      // "iDisplayLength": 20,
+      "info": true,
+      "aria": false,
+      search: {
+            return: true,
+        },
+        "language": {
+         "search": "輸入關鍵字",
+         "zeroRecords": "找不到您搜尋的相關店面！",
+         "info": "顯示 _PAGES_ 頁中的第 _PAGE_ 頁",
+         "lengthMenu": "顯示 _MENU_ 則結果",
+         "infoEmpty":      "",
+         "infoFiltered": "(搜尋 _MAX_ 中的結果)",
+         "paginate": {
+            "first":      "第一頁",
+            "last":       "尾頁",
+            "next":       "下一頁",
+            "previous":   "上一頁"
+        },
+      }
+    });
+});
 
 
 document.addEventListener('DOMContentLoaded', function(){
